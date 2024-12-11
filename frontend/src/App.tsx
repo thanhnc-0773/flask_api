fda32225944679fca2a2cced168c8f55f45da6f2
nguyen.dinh.minh.tuan <nguyen.dinh.minh.tuan@sun-asterisk.com>
2024-12-12 00:41:27 +0700

    Update reset scroll

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import RoutesApp from "./routes/index.routes";
+import ResetScroll from "./components/atoms/ResetScroll";
 
 function App() {
   return (
     <>
+      <ResetScroll />
       <RoutesApp />
     </>
   );
